9cd1809f978687bc0967363c308a33b17e7fb434
TokenPoker <95186945+TokenPoker@users.noreply.github.com>
2025-04-11 15:03:23 +0200

    commit remise à neuf

--- sources/pieces.h
+++ sources/pieces.h
@@ -1,7 +1,4 @@
 
-#include "pieces.h"
-#include <string.h> // pour memcpy
-
 #define MAX_PIECE_SIZE 5
 #define MAX_PIECES 7
 
@@ -12,37 +9,4 @@ typedef struct{
     int height;
 }Piece;
 
-<<<<<<< HEAD:sources/pieces.h
 int load_pieces_from_file(const char* filename, Piece* pieces);
-=======
-
-
-void rotate_piece(Piece *p, int angle) {
-    char temp[MAX_PIECEa_SIZE][MAX_PIECE_SIZE]; // Piece temporaire qui ca réceptionner la pièce tournée. 
-    int n = MAX_PIECE_SIZE;
-
-    // Nombre de rotations à 90° (ex : 180° = 2 fois)
-    int num_rotations = (angle / 90) % 4;
-
-    for (int r = 0; r < num_rotations; r++) {
-        // Rotation à 90° dans temp
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[j][n - 1 - i] = p->shape[i][j];
-            }
-        }
-
-        // Copie de temp dans shape
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                p->shape[i][j] = temp[i][j];
-            }
-        }
-
-        // Mettre à jour width/height
-        int tmp = p->width;
-        p->width = p->height;
-        p->height = tmp;
-    }
-}
->>>>>>> c619189 (Rotationpiece):sources/pieces_management.c
